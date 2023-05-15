60643eb6c58401ee2dc299270b34c0b1bde147f0
jagoda-kubicka <jagoda.kubicka@estorelabs.com>
2023-05-15 14:17:10 +0200

    magnifing glass section

--- app.js
+++ app.js
@@ -71,11 +71,11 @@ esShowContentButtons.forEach(esShowContentButton=> {
 
 //calculator
 
-const calculatorSubmitBtn = document.querySelector('.es-calculator__submitBtn');
-const calculatorReturnBtn = document.querySelector('.es-calculator__returnBtn');
+const calculatorSubmitBtn = document.querySelector('#es-sanofi.es-sanofi #es-magne.es-magne .es-calculator__submitBtn');
+const calculatorReturnBtn = document.querySelector('#es-sanofi.es-sanofi #es-magne.es-magne .es-calculator__returnBtn');
 
-const calculatorInputsCtn = document.querySelector('.es-calculator__ctn--inputs');
-const calculatorResutlsCtn = document.querySelector('.es-calculator__ctn--results');
+const calculatorInputsCtn = document.querySelector('#es-sanofi.es-sanofi #es-magne.es-magne .es-calculator__ctn--inputs');
+const calculatorResutlsCtn = document.querySelector('#es-sanofi.es-sanofi #es-magne.es-magne .es-calculator__ctn--results');
 
 
 const showCalculatorResults= (input, result) => {
@@ -91,6 +91,71 @@ const returnToCalculator= (input, result) => {
 calculatorSubmitBtn.addEventListener('click', () => showCalculatorResults(calculatorInputsCtn, calculatorResutlsCtn));
 calculatorReturnBtn.addEventListener('click', () => returnToCalculator(calculatorInputsCtn, calculatorResutlsCtn));
 
+
+//magnifier glass
+
+function esMagnify(imgID, zoom) {
+  var img, glass, w, h, bw;
+  img = document.getElementById(imgID);
+
+  /* Create magnifier glass: */
+  glass = document.createElement("DIV");
+  glass.setAttribute("class", "es-symptoms__magnifierGlass");
+
+  /* Insert magnifier glass: */
+  img.parentElement.insertBefore(glass, img);
+
+  /* Set background properties for the magnifier glass: */
+  glass.style.backgroundImage = "url('" + img.src + "')";
+  glass.style.backgroundRepeat = "no-repeat";
+  glass.style.backgroundSize = (img.width * zoom) + "px " + (img.height * zoom) + "px";
+  bw = 3;
+  w = glass.offsetWidth / 2;
+  h = glass.offsetHeight / 2;
+
+  /* Execute a function when someone moves the magnifier glass over the image: */
+  glass.addEventListener("mousemove", esMoveMagnifier);
+  img.addEventListener("mousemove", esMoveMagnifier);
+
+  /*and also for touch screens:*/
+  glass.addEventListener("touchmove", esMoveMagnifier);
+  img.addEventListener("touchmove", esMoveMagnifier);
+  function esMoveMagnifier(e) {
+    var pos, x, y;
+    /* Prevent any other actions that may occur when moving over the image */
+    e.preventDefault();
+    /* Get the cursor's x and y positions: */
+    pos = getCursorPos(e);
+    x = pos.x;
+    y = pos.y;
+    /* Prevent the magnifier glass from being positioned outside the image: */
+    if (x > img.width - (w / zoom)) {x = img.width - (w / zoom);}
+    if (x < w / zoom) {x = w / zoom;}
+    if (y > img.height - (h / zoom)) {y = img.height - (h / zoom);}
+    if (y < h / zoom) {y = h / zoom;}
+    /* Set the position of the magnifier glass: */
+    glass.style.left = (x - w) + "px";
+    glass.style.top = (y - h) + "px";
+    /* Display what the magnifier glass "sees": */
+    glass.style.backgroundPosition = "-" + ((x * zoom) - w + bw) + "px -" + ((y * zoom) - h + bw) + "px";
+  }
+
+  function getCursorPos(e) {
+    var a, x = 0, y = 0;
+    e = e || window.event;
+    /* Get the x and y positions of the image: */
+    a = img.getBoundingClientRect();
+    /* Calculate the cursor's x and y coordinates, relative to the image: */
+    x = e.pageX - a.left;
+    y = e.pageY - a.top;
+    /* Consider any page scrolling: */
+    x = x - window.pageXOffset;
+    y = y - window.pageYOffset;
+    return {x : x, y : y};
+  }
+}
+
+esMagnify("esBodyImg", 1.4);
 
 
 //faq
