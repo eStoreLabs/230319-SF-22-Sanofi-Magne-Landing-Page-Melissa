a1a08f57dd9c46c49b43e5e71056ee900faa1837
Grzegorz Sulich <greg.sulich@estorelabs.com>
2023-05-16 15:34:07 +0200

    add auto zoom to conditions display

--- app.js
+++ app.js
@@ -157,68 +157,43 @@ esDateOption.valueAsDate = new Date()
 
 //magnifier glass
 
-function esMagnify(imgID, zoom) {
-  var img, glass, w, h, bw;
-  img = document.getElementById(imgID);
+const zoomGlassPositions = [
+    [50, 12],
+    [50, 16],
+    [50, 14],
+    [50, 33],
+    [50, 23],
+    [61, 26],
+    [50, 40],
+    [60, 79],
+]
+let currentMagnifiedItem = -1;
+const magnifierGlass = document.querySelector('#es-sanofi.es-sanofi #es-magne.es-magne .es-symptoms__magnifierGlass')
 
-  /* Create magnifier glass: */
-  glass = document.createElement("DIV");
-  glass.setAttribute("class", "es-symptoms__magnifierGlass");
+function esMagnifyNextItem() {
+  currentMagnifiedItem++
 
-  /* Insert magnifier glass: */
-  img.parentElement.insertBefore(glass, img);
+  const allItems = document.querySelectorAll('#es-sanofi.es-sanofi #es-magne.es-magne .es-symptoms__contentCtn .es-symptoms__singleItem')
+  const allZoomImages = document.querySelectorAll('#es-sanofi.es-sanofi #es-magne.es-magne .es-symptoms__magnifierGlass img')
 
-  /* Set background properties for the magnifier glass: */
-  glass.style.backgroundImage = "url('" + img.src + "')";
-  glass.style.backgroundRepeat = "no-repeat";
-  glass.style.backgroundSize = (img.width * zoom) + "px " + (img.height * zoom) + "px";
-  bw = 3;
-  w = glass.offsetWidth / 2;
-  h = glass.offsetHeight / 2;
-
-  /* Execute a function when someone moves the magnifier glass over the image: */
-  glass.addEventListener("mousemove", esMoveMagnifier);
-  img.addEventListener("mousemove", esMoveMagnifier);
-
-  /*and also for touch screens:*/
-  glass.addEventListener("touchmove", esMoveMagnifier);
-  img.addEventListener("touchmove", esMoveMagnifier);
-  function esMoveMagnifier(e) {
-    var pos, x, y;
-    /* Prevent any other actions that may occur when moving over the image */
-    e.preventDefault();
-    /* Get the cursor's x and y positions: */
-    pos = getCursorPos(e);
-    x = pos.x;
-    y = pos.y;
-    /* Prevent the magnifier glass from being positioned outside the image: */
-    if (x > img.width - (w / zoom)) {x = img.width - (w / zoom);}
-    if (x < w / zoom) {x = w / zoom;}
-    if (y > img.height - (h / zoom)) {y = img.height - (h / zoom);}
-    if (y < h / zoom) {y = h / zoom;}
-    /* Set the position of the magnifier glass: */
-    glass.style.left = (x - w) + "px";
-    glass.style.top = (y - h) + "px";
-    /* Display what the magnifier glass "sees": */
-    glass.style.backgroundPosition = "-" + ((x * zoom) - w + bw) + "px -" + ((y * zoom) - h + bw) + "px";
+  if (currentMagnifiedItem >= allItems.length) {
+    currentMagnifiedItem = 0
   }
+  allItems.forEach((el) => {
+    el.classList.remove('es-currentlySelected')
+  })
+  allZoomImages.forEach((el) => {
+    el.style.opacity = 0
+  })
 
-  function getCursorPos(e) {
-    var a, x = 0, y = 0;
-    e = e || window.event;
-    /* Get the x and y positions of the image: */
-    a = img.getBoundingClientRect();
-    /* Calculate the cursor's x and y coordinates, relative to the image: */
-    x = e.pageX - a.left;
-    y = e.pageY - a.top;
-    /* Consider any page scrolling: */
-    x = x - window.pageXOffset;
-    y = y - window.pageYOffset;
-    return {x : x, y : y};
-  }
+  allItems.item(currentMagnifiedItem).classList.add('es-currentlySelected')
+  allZoomImages.item(currentMagnifiedItem).style.opacity = 1
+  magnifierGlass.style.left = `${zoomGlassPositions[currentMagnifiedItem][0]}%`
+  magnifierGlass.style.top = `${zoomGlassPositions[currentMagnifiedItem][1]}%`
+
+  setTimeout(esMagnifyNextItem, 3000)
 }
-
-esMagnify("esBodyImg", 1.4);
+esMagnifyNextItem()
 
 
 //faq
