f04d581caefb6266dc56686c24791049e92f1cb8
jagoda-kubicka <jagoda.kubicka@estorelabs.com>
2023-06-06 12:25:12 +0200

    swiper sliders

--- app.js
+++ app.js
@@ -33,89 +33,77 @@
   navBtn.addEventListener("click", () => toggleMenu());
   //slider
 
-  const esNext = document.querySelector(
-    "#es-sanofi.es-sanofi #es-magne.es-magne .es-next"
-  );
-  const esPrev = document.querySelector(
-    "#es-sanofi.es-sanofi #es-magne.es-magne .es-prev"
-  );
+  // const esNext2 = document.querySelector(
+  //   "#es-sanofi.es-sanofi #es-magne.es-magne .es-next2"
+  // );
+  // const esPrev2 = document.querySelector(
+  //   "#es-sanofi.es-sanofi #es-magne.es-magne .es-prev2"
+  // );
 
-  const esNext2 = document.querySelector(
-    "#es-sanofi.es-sanofi #es-magne.es-magne .es-next2"
-  );
-  const esPrev2 = document.querySelector(
-    "#es-sanofi.es-sanofi #es-magne.es-magne .es-prev2"
-  );
+  // const esNext3 = document.querySelector(
+  //   "#es-sanofi.es-sanofi #es-magne.es-magne .es-next3"
+  // );
+  // const esPrev3 = document.querySelector(
+  //   "#es-sanofi.es-sanofi #es-magne.es-magne .es-prev3"
+  // );
 
-  const esNext3 = document.querySelector(
-    "#es-sanofi.es-sanofi #es-magne.es-magne .es-next3"
-  );
-  const esPrev3 = document.querySelector(
-    "#es-sanofi.es-sanofi #es-magne.es-magne .es-prev3"
-  );
+  // const esNext4 = document.querySelector(
+  //   "#es-sanofi.es-sanofi #es-magne.es-magne .es-next4"
+  // );
+  // const esPrev4 = document.querySelector(
+  //   "#es-sanofi.es-sanofi #es-magne.es-magne .es-prev4"
+  // );
 
-  const esNext4 = document.querySelector(
-    "#es-sanofi.es-sanofi #es-magne.es-magne .es-next4"
-  );
-  const esPrev4 = document.querySelector(
-    "#es-sanofi.es-sanofi #es-magne.es-magne .es-prev4"
-  );
+  // const esNext5 = document.querySelector(
+  //   "#es-sanofi.es-sanofi #es-magne.es-magne .es-next5"
+  // );
+  // const esPrev5 = document.querySelector(
+  //   "#es-sanofi.es-sanofi #es-magne.es-magne .es-prev5"
+  // );
 
-  const esNext5 = document.querySelector(
-    "#es-sanofi.es-sanofi #es-magne.es-magne .es-next5"
-  );
-  const esPrev5 = document.querySelector(
-    "#es-sanofi.es-sanofi #es-magne.es-magne .es-prev5"
-  );
-
-  let slideIndex = [1, 1, 1, 1, 1];
+  // let slideIndex = [1, 1, 1, 1];
   /* Class the members of each slideshow group with different CSS classes */
-  let slideId = [
-    "es-mySlides",
-    "es-mySlides-2",
-    "es-mySlides-3",
-    "es-mySlides-4",
-    "es-mySlides-5",
-  ];
-  showSlides(1, 0);
-  showSlides(1, 1);
-  showSlides(1, 2);
-  showSlides(1, 3);
-  showSlides(1, 4);
+  // let slideId = [
+  //   "es-mySlides-2",
+  //   "es-mySlides-3",
+  //   "es-mySlides-4",
+  //   "es-mySlides-5",
+  // ];
+  // showSlides(1, 1);
+  // showSlides(1, 2);
+  // showSlides(1, 3);
+  // showSlides(1, 4);
 
-  function plusSlides(n, no) {
-    showSlides((slideIndex[no] += n), no);
-  }
+  // function plusSlides(n, no) {
+  //   showSlides((slideIndex[no] += n), no);
+  // }
 
-  function showSlides(n, no) {
-    let i;
-    let x = document.getElementsByClassName(slideId[no]);
-    if (n > x.length) {
-      slideIndex[no] = 1;
-    }
-    if (n < 1) {
-      slideIndex[no] = x.length;
-    }
-    for (i = 0; i < x.length; i++) {
-      x[i].style.display = "none";
-    }
-    x[slideIndex[no] - 1].style.display = "block";
-  }
+  // function showSlides(n, no) {
+  //   let i;
+  //   let x = document.getElementsByClassName(slideId[no]);
+  //   if (n > x.length) {
+  //     slideIndex[no] = 1;
+  //   }
+  //   if (n < 1) {
+  //     slideIndex[no] = x.length;
+  //   }
+  //   for (i = 0; i < x.length; i++) {
+  //     x[i].style.display = "none";
+  //   }
+  //   x[slideIndex[no] - 1].style.display = "block";
+  // }
 
-  esNext.addEventListener("click", () => plusSlides(1, 0));
-  esPrev.addEventListener("click", () => plusSlides(-1, 0));
+  // esNext2.addEventListener("click", () => plusSlides(1, 1));
+  // esPrev2.addEventListener("click", () => plusSlides(-1, 1));
 
-  esNext2.addEventListener("click", () => plusSlides(1, 1));
-  esPrev2.addEventListener("click", () => plusSlides(-1, 1));
+  // esNext3.addEventListener("click", () => plusSlides(1, 2));
+  // esPrev3.addEventListener("click", () => plusSlides(-1, 2));
 
-  esNext3.addEventListener("click", () => plusSlides(1, 2));
-  esPrev3.addEventListener("click", () => plusSlides(-1, 2));
+  // esNext4.addEventListener("click", () => plusSlides(1, 3));
+  // esPrev4.addEventListener("click", () => plusSlides(-1, 3));
 
-  esNext4.addEventListener("click", () => plusSlides(1, 3));
-  esPrev4.addEventListener("click", () => plusSlides(-1, 3));
-
-  esNext5.addEventListener("click", () => plusSlides(1, 4));
-  esPrev5.addEventListener("click", () => plusSlides(-1, 4));
+  // esNext5.addEventListener("click", () => plusSlides(1, 4));
+  // esPrev5.addEventListener("click", () => plusSlides(-1, 4));
 
   //show content
 
